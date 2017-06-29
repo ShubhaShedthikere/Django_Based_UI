5951caf2ba98f5d9c50add1723e69e42284cc5cf
ShubhaShedthikere <shubha.shedthikere@gmail.com>
2017-06-29 16:57:02 +0530

    urls file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+
     url(r'^traffic/', include('traffic_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
